83f916882148c18d807e0fb05137a5fcf41f6f35
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2023-08-02 11:37:29 +0500

    init

--- db/db.config.js
+++ db/db.config.js
@@ -2,7 +2,7 @@ const { Sequelize, DataTypes } = require("sequelize")
 const dotenv = require("dotenv")
 
 dotenv.config()
-const sequelize = new Sequelize("postgres://default:************@ep-gentle-violet-77044958.us-east-1.postgres.vercel-storage.com:5432/verceldb",{
+const sequelize = new Sequelize("postgres://default:rxpPGSMyY5F2@ep-gentle-violet-77044958.us-east-1.postgres.vercel-storage.com:5432/verceldb",{
     logging: false
 })
 
