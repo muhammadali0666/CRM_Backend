6159e489b084398dce689cdba31109b2e228c8e2
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2023-08-09 08:48:30 +0500

    added some changes

--- db/db.config.js
+++ db/db.config.js
@@ -2,17 +2,15 @@ const { Sequelize, DataTypes } = require("sequelize")
 const dotenv = require("dotenv")
 
 dotenv.config()
-const sequelize = new Sequelize("postgres://sdkflopp:FyrhQRIVFX9Fs1hsLg6IifEH4BzCyQL_@floppy.db.elephantsql.com/sdkflopp",{
+const sequelize = new Sequelize("postgres://sdkflopp:FyrhQRIVFX9Fs1hsLg6IifEH4BzCyQL_@floppy.db.elephantsql.com/sdkflopp", {
     logging: false
 })
 
 sequelize
-      .authenticate()
-      .then(() => console.log('Connected'))
-      .catch((err) => console.log(err))  
-
-
-
+    .authenticate()
+    .then(() => console.log('Connected'))
+    .catch((err) => console.log(err))
+    
 module.exports = {
     sequelize,
     DataTypes
